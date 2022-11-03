17683884c56efdb1481043e37061fc7538a91be5
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2022-11-03 09:30:23 -0700

    Did stuff
    weee

---
+++ RubysAdventureGonzalez/Assets/Scripts/MovingRuby.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRuby : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = 
+        
+    }
+}
