6c3e06acc49e5dde900cc2384f5515926aa98109
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2022-11-07 09:56:51 -0800

    Made Ruby Move Code
    Ruby does not move

--- RubysAdventureGonzalez/Assets/Scripts/MovingRuby.cs
+++ RubysAdventureGonzalez/Assets/Scripts/MovingRuby.cs
@@ -7,14 +7,14 @@ public class MovingRuby : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = 
-        
+        Debug.Log(horizontal);
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        transform.position = position;
     }
 }
