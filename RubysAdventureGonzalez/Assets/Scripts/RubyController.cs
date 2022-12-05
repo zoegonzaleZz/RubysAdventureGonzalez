3cf22bce87aa89c0fda687b6746dcd38beb75904
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2022-12-05 09:56:48 -0800

    idk
    nick is annoying

--- RubysAdventureGonzalez/Assets/Scripts/RubyController.cs
+++ RubysAdventureGonzalez/Assets/Scripts/RubyController.cs
@@ -12,9 +12,10 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
